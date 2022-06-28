7955760f6c4c4f5ff24f868280117738c5163c85
e2662020 <63095313+e2662020@users.noreply.github.com>
2022-06-28 17:04:11 +0800

    6/28/2022/17/04

---
+++ severs/main.sh
@@ -0,0 +1,8 @@
+cd /Users/ericsmac/Desktop/severs
+echo "# minemyfuturesever" >> README.md
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:e2662020/minemyfuturesever.git
+git push -u origin main
